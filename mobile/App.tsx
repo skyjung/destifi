43c48e0634bebd513ca4a0ba402d45baf895ccf0
Sky <skyjung@college.harvard.edu>
2022-04-16 15:41:21 -0400

    loaded poppins font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import * as Font from 'expo-font';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-
+const fetchFonts = () =>
+  Font.loadAsync({
+    'poppins': require('./assets/fonts/Poppins-Regular.ttf'),
+  });
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -12,6 +15,7 @@ export default function App() {
   if (!isLoadingComplete) {
     return null;
   } else {
+    fetchFonts();
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
--- mobile/components/Themed.tsx
+++ mobile/components/Themed.tsx
@@ -3,8 +3,8 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import { Text as DefaultText, View as DefaultView } from 'react-native';
 
+import { Text as DefaultText, View as DefaultView } from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 
@@ -34,7 +34,7 @@ export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+  return <DefaultText style={[{ color }, style, {fontFamily:"poppins"}]} {...otherProps} />;
 }
 
 export function View(props: ViewProps) {
