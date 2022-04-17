a79dd3cc4dacac4c350cf9a34899c18e21d4d078
nathanzzhang <70558078+nathanzzhang@users.noreply.github.com>
2022-04-16 20:19:56 -0700

    updated colors and visuals

--- mobile/screens/LoginScreen.tsx
+++ mobile/screens/LoginScreen.tsx
@@ -3,28 +3,36 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import { Button } from 'react-native-paper';
+import { Button, Headline } from 'react-native-paper';
 import { RootTabScreenProps } from '../types';
 import { TextInput } from 'react-native-paper';
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
 
 export default function LoginScreen({ navigation }: RootTabScreenProps<'Login'>) {
+    const colorScheme = useColorScheme();
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Log In</Text>
+            <Headline style={styles.title}>log in</Headline>
             <View style={styles.input}>
-                <TextInput label="Username"
-                           placeholder="Username" />
+                <TextInput label="username"
+                           mode='outlined'
+                           outlineColor={Colors[colorScheme].tint}
+                           placeholder="username" />
             </View>
             <View style={styles.input}>
-                <TextInput label="Password"
+                <TextInput label="password"
+                           mode='outlined'
+                           outlineColor={Colors[colorScheme].tint}
                            secureTextEntry
                            right={<TextInput.Icon name="eye" />}/>
             </View>
             <View style={styles.bottom}>
-                <Button mode="contained" onPress={() => navigation.navigate("Root", { screen : "Home" })}>log in</Button>
+                <Button uppercase={false} mode="contained" onPress={() => navigation.navigate("Root", { screen : "Home" })}>log in</Button>
             </View>
             <View style={styles.bottom}>
-                <Button mode="outlined" onPress={() => navigation.navigate("Landing")}>back</Button>
+                <Button uppercase={false} mode="outlined" onPress={() => navigation.navigate("Landing")}>back</Button>
             </View>
         </View>
     );
--- mobile/screens/SignupScreen.tsx
+++ mobile/screens/SignupScreen.tsx
@@ -3,37 +3,49 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import { Button } from 'react-native-paper';
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import { Button, Headline } from 'react-native-paper';
 import { RootTabScreenProps } from '../types';
 import { TextInput } from 'react-native-paper';
 
 export default function SignupScreen({ navigation }: RootTabScreenProps<'Signup'>) {
+    const colorScheme = useColorScheme();
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Sign Up</Text>
+            <Headline style={styles.title}>sign up</Headline>
             <View style={styles.input}>
-                <TextInput label="Email"
+                <TextInput label="name"
+                           mode='outlined'
+                           outlineColor={Colors[colorScheme].tint}
+                           placeholder="Name" />
+            </View>
+            <View style={styles.input}>
+                <TextInput label="email"
+                           mode='outlined'
+                           outlineColor={Colors[colorScheme].tint}
                            placeholder="Email" />
             </View>
             <View style={styles.input}>
-                <TextInput label="Username"
-                           placeholder="Username" />
-            </View>
-            <View style={styles.input}>
-                <TextInput label="Password"
+                <TextInput label="password"
+                           mode='outlined'
+                           outlineColor={Colors[colorScheme].tint}
                            secureTextEntry
                            right={<TextInput.Icon name="eye" />}/>
             </View>
             <View style={styles.input}>
-                <TextInput label="Confirm Password"
+                <TextInput label="confirm password"
+                           mode='outlined'
+                           outlineColor={Colors[colorScheme].tint}
                            secureTextEntry
                            right={<TextInput.Icon name="eye" />}/>
             </View>
             <View style={styles.bottom}>
-                <Button mode="contained" onPress={() => navigation.navigate("Root", { screen : "Home" })}>log in</Button>
+                <Button uppercase={false} mode="contained" onPress={() => navigation.navigate("Root", { screen : "Home" })}>log in</Button>
             </View>
             <View style={styles.bottom}>
-                <Button mode="outlined" onPress={() => navigation.navigate("Landing")}>back</Button>
+                <Button uppercase={false} mode="outlined" onPress={() => navigation.navigate("Landing")}>back</Button>
             </View>
         </View>
     );
@@ -51,10 +63,12 @@ const styles = StyleSheet.create({
         fontSize: 35,
         fontWeight: 'bold',
         padding: 15,
+        textTransform: "none",
     },
     input: {
         height: 75,
         padding: 10,
+        borderRadius: 5,
     },
     bottom : {
         paddingTop: 25,
